07f43dda47a1c30cd6722598e531bc62b10657e0
Ivan Donat Pupovac <ivandonat@gmail.com>
2018-11-17 14:56:38 +0000

    Implement Node Add

--- src/main/java/com/durhack/sharpshot/nodes/Container.java
+++ src/main/java/com/durhack/sharpshot/nodes/Container.java
@@ -1,21 +1,35 @@
 package com.durhack.sharpshot.nodes;
 
 import com.durhack.sharpshot.Bullet;
+import com.durhack.sharpshot.Direction;
 import com.durhack.sharpshot.INode;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
+import java.math.BigInteger;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Container implements INode {
+    private Direction dir = Direction.UP;
+
     private INode[][] nodes;
     private List<Bullet> bullets;
 
 
+    @Override
+    public @NotNull Direction getRotation() {
+        return dir;
+    }
+
+    @Override
+    public void rotateClockwise() {
+        dir = Direction.clockwiseOf(dir);
+    }
+
     @NotNull
     @Override
-    public List<Bullet> run(@NotNull Bullet bullet) {
-        return new ArrayList<>();
+    public Map<Direction, BigInteger> run(@NotNull Bullet bullet) {
+        return new HashMap<>();
     }
 }
