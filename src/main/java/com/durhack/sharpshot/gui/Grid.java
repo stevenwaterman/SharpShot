2e6f628a7fa393a05411309df6a2ba3d25a83164
Steven Lowes <stevenhlowes@gmail.com>
2018-11-17 17:55:12 +0000

    Add animation button

--- src/main/java/com/durhack/sharpshot/gui/Grid.java
+++ src/main/java/com/durhack/sharpshot/gui/Grid.java
@@ -4,38 +4,38 @@ import com.durhack.sharpshot.Bullet;
 import com.durhack.sharpshot.Coordinate;
 import com.durhack.sharpshot.Direction;
 import com.durhack.sharpshot.INode;
-import com.durhack.sharpshot.nodes.Container;
-import com.durhack.sharpshot.nodes.NodeAdd;
+import com.durhack.sharpshot.nodes.*;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import org.jetbrains.annotations.NotNull;
 
 import java.math.BigInteger;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class Grid extends Application {
     private GridPane pane = new GridPane();
     private Container container;
 
     public Grid() {
-        container = new Container(10, 5);
+        container = new Container(50, 30);
         container.getBullets().put(new Coordinate(1, 3), new Bullet(Direction.DOWN, BigInteger.ONE));
         container.getBullets().put(new Coordinate(1, 4), new Bullet(Direction.DOWN, BigInteger.ONE));
         container.getNodes().put(new Coordinate(1, 2), new NodeAdd());
+        container.getNodes().put(new Coordinate(2, 2), new NodeSub());
+        container.getNodes().put(new Coordinate(3, 2), new NodeMult());
+        container.getNodes().put(new Coordinate(4, 2), new NodeDiv());
     }
 
     private void render() {
@@ -79,10 +79,24 @@ public class Grid extends Application {
         BorderPane borderPane = new BorderPane();
         borderPane.setTop(pane);
 
-        Button button = new Button("Tick");
-        button.setOnAction(actionEvent -> tick());
-        pane.add(button, 0, container.getHeight());
-        borderPane.setBottom(button);
+        Button tickButton = new Button("Tick");
+        tickButton.setOnAction(actionEvent -> tick());
+        pane.add(tickButton, 0, container.getHeight());
+
+        Button runButton = new Button("Run");
+        runButton.setOnAction(actionEvent -> {
+            runButton.setDisable(true);
+            Timer timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    Platform.runLater(() -> tick());
+                }
+            }, 0, 250);
+        });
+
+        HBox hBox = new HBox(tickButton, runButton);
+        borderPane.setBottom(hBox);
 
         Scene rootScene = new Scene(borderPane);
         primaryStage.setScene(rootScene);
