70bfc2d20106bf67a9d64625f5eff3db81ac7218
Steven Lowes <motherlymuppet@users.noreply.github.com>
2018-11-23 19:42:46 +0000

    Simplify collision code

--- src/main/java/com/durhack/sharpshot/logic/Container.kt
+++ src/main/java/com/durhack/sharpshot/logic/Container.kt
@@ -4,7 +4,7 @@ import com.durhack.sharpshot.nodes.INode
 import com.durhack.sharpshot.nodes.io.AbstractInputNode
 import com.durhack.sharpshot.nodes.other.HaltNode
 import javafx.beans.property.SimpleBooleanProperty
-import tornadofx.observable
+import tornadofx.*
 import java.math.BigInteger
 
 class Container(val width: Int, val height: Int) {
@@ -13,7 +13,7 @@ class Container(val width: Int, val height: Int) {
     val nodes = mutableMapOf<Coordinate, INode>().observable()
     val bullets = mutableMapOf<Coordinate, Bullet>().observable()
 
-    var halt = false
+    private var halt = false
 
     fun start(input: List<BigInteger?>) {
         bullets.putAll(nodes.keys.flatMap { coordinate ->
@@ -33,14 +33,54 @@ class Container(val width: Int, val height: Int) {
      * all bullets -> check
      */
     fun tick() {
-        val movements = moveBullets().toMutableList()
+        val movements = moveBullets()
+        val swapsCollided = collideSwaps(movements)
+        val finalCollided = collideFinal(swapsCollided)
 
+        //Move bullets
+        val newBullets = finalCollided.map { (movement, bullet) ->
+            movement.to to bullet
+        }.toMap()
+
+        //replace old bullets with new bullets
+        bullets.clear()
+        bullets.putAll(newBullets)
+
+        //Halt if we hit a halt node
+        if (halt) {
+            reset()
+        }
+    }
+
+    private fun collideFinal(movements: Map<Movement, Bullet>): Map<Movement, Bullet> {
+        //Remove bullets that end in the same place
+        val collideProcessed = mutableSetOf<Coordinate>()
+        val collideDelete = mutableSetOf<Coordinate>()
+
+        movements.forEach { (movement, _) ->
+            val to = movement.to
+            if (to !in collideDelete) {
+                if (to in collideProcessed) {
+                    collideProcessed.remove(to)
+                    collideDelete.add(to)
+                }
+                else {
+                    collideProcessed.add(to)
+                }
+            }
+        }
+
+        return movements.filterNot { it.key.to in collideDelete }.toMap()
+    }
+
+    private fun collideSwaps(movements: Map<Movement, Bullet>): Map<Movement, Bullet> {
         //Remove swapping bullets
         //When two bullets are adjacent and each is trying to move into the space the other is occupying, a naive solution
         //would allow them to swap without colliding.
         //This solves that problem
         val swapProcessed = mutableSetOf<SwapChecker>()
         val swapDelete = mutableSetOf<Coordinate>()
+
         movements.map { (movement, _) ->
             SwapChecker(movement)
         }.forEach { swap ->
@@ -56,43 +96,10 @@ class Container(val width: Int, val height: Int) {
             }
         }
 
-        movements.removeIf { (movement, _) ->
-            swapDelete.contains(movement.from)
-        }
-
-        //Remove bullets that end in the same place
-        val collideProcessed = mutableSetOf<Coordinate>()
-        val collideDelete = mutableSetOf<Coordinate>()
-        movements.forEach { (movement, _) ->
-            val to = movement.to
-            if (to !in collideDelete) {
-                if (to in collideProcessed) {
-                    collideProcessed.remove(to)
-                    collideDelete.add(to)
-                }
-                else {
-                    collideProcessed.add(to)
-                }
-            }
-        }
-        movements.removeIf { (movement, _) -> collideDelete.contains(movement.to) }
-
-        //Move bullets
-        val newBullets = movements.map { (movement, bullet) ->
-            movement.to to bullet
-        }.toMap()
-
-        //replace old bullets with new bullets
-        bullets.clear()
-        bullets.putAll(newBullets)
-
-        //Halt if we hit a halt node
-        if (halt) {
-            reset()
-        }
+        return movements.filterNot { it.key.from in swapDelete }
     }
 
-    private fun moveBullets(): List<Pair<Movement, Bullet>> {
+    private fun moveBullets(): Map<Movement, Bullet> {
         //Bullets on nodes
         val captured = bullets.mapNotNull { (coord, bullet) ->
             val node = nodes[coord] ?: return@mapNotNull null
@@ -104,7 +111,7 @@ class Container(val width: Int, val height: Int) {
         return moveBullets(freeBullets) + processBullets(captured)
     }
 
-    private fun processBullets(captured: List<Triple<Coordinate, INode, Bullet>>) =
+    private fun processBullets(captured: List<Triple<Coordinate, INode, Bullet>>): Map<Movement, Bullet> =
             captured.flatMap { (coord, node, bullet) ->
                 // special case
                 // if any halt nodes get hit freeBulletLocations.add(by a bullet, halt at end of
@@ -124,12 +131,13 @@ class Container(val width: Int, val height: Int) {
 
                     return@map movement to newBullet
                 }
-            }
+            }.toMap()
 
-    private fun moveBullets(bullets: Map<Coordinate, Bullet>) = bullets.map { (coord, bullet) ->
-        val newCoord = coord.plus(bullet.direction).wrap(width, height)
-        return@map Movement(coord, newCoord) to bullet
-    }
+    private fun moveBullets(bullets: Map<Coordinate, Bullet>): Map<Movement, Bullet> =
+            bullets.map { (coord, bullet) ->
+                val newCoord = coord.plus(bullet.direction).wrap(width, height)
+                return@map Movement(coord, newCoord) to bullet
+            }.toMap()
 
     fun reset() {
         bullets.clear()
